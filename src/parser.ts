cca3e43232fa619a258f66320ca3e37b4d6a8956
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-07-29 16:18:43 +0800

    Parse the nginx full example

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,4 @@
-import { createToken, Lexer, CstParser, tokenMatcher, EOF, CstNode } from "chevrotain";
+import { createToken, Lexer, CstParser, tokenMatcher, CstNode, Rule } from "chevrotain";
 
 class FormulaError extends Error {
     public details: any;
@@ -9,83 +9,109 @@ class FormulaError extends Error {
 }
 
 class ParserError extends FormulaError { }
+const LCurly = createToken({ name: "LCurly", pattern: /{/ });
+const RCurly = createToken({ name: "RCurly", pattern: /}/ });
+const Semicolon = createToken({ name: "Semicolon", pattern: /;/ });
+const SingleQuote = createToken({ name: "single quote", pattern: "'" });
+const DoubleQuote = createToken({ name: "double quote", pattern: /"/ });
+const Comment = createToken({
+    name: "Comment",
+    pattern: /#[^\n\r]*/,
+    group: Lexer.SKIPPED
+});
+const Literal = createToken({
+    name: "Literal", pattern: /[^{};]+/
+});
 
-export function parse(entry: string): any {
-    const commonWord = createToken({ name: "commonWord", pattern: /\w+/ });
-    const keyword = createToken({ name: "keyword", pattern: /\*\*\w+\*\*/ });
-    const punctuation = createToken({ name: "punctuation", pattern: /[.,?!'\";:-]/ });
-    const whitespace = createToken({ name: "whitespace", pattern: /\s+/, group: Lexer.SKIPPED });
-    const allTokens = [
-        whitespace,
-        commonWord,
-        keyword,
-        punctuation
-    ];
-    class ExcerptParser extends CstParser {
-        constructor() {
-            super(allTokens);
-            this.performSelfAnalysis();
-        }
-        public excerpt = this.RULE("excerpt", () => {
-            this.SUBRULE(this.wordList, { LABEL: "wordList" });
-        }
-        );
+const WhiteSpace = createToken({
+    name: "WhiteSpace",
+    pattern: /[ \t]+/,
+    group: Lexer.SKIPPED
+});
 
-        private wordList = this.RULE("wordList", () => {
-            this.MANY(() => this.SUBRULE(this.word, { LABEL: "list" }));
-        });
+const LineTerminator = createToken({
+    name: "LineTerminator",
+    pattern: /\n\r|\r|\n/,
+    group: Lexer.SKIPPED
+});
 
-        private word = this.RULE("word", () => {
-            this.OR([
-                { ALT: () => this.CONSUME(commonWord) },
-                { ALT: () => this.CONSUME(keyword) },
-                { ALT: () => this.CONSUME(punctuation) }
-            ]);
-        });
-    };
-    const parser = new ExcerptParser();
-    const excerptVisitor = parser.getBaseCstVisitorConstructor();
-    class ExceptInterpreter extends excerptVisitor {
-        constructor() {
-            super();
-            this.validateVisitor();
-        }
-        excerpt(ctx: { wordList: CstNode | CstNode[]; }): any {
-            return this.visit(ctx.wordList);
-        }
-        wordList(ctx: { list: any[]; }): any {
-            let wordList: Array<string> = [];
-            let keywords: Array<string> = [];
-            ctx.list.forEach((word) => {
-                let w = this.visit(word);
-                wordList.push(w.image);
-                if (tokenMatcher(w, keyword)) {
-                    keywords.push(w.image);
-                }
-            });
-            return [keywords, wordList];
-        }
-
-        word(ctx: { commonWord: any[]; keyword: any[]; punctuation: any[]; }): any {
-            if (ctx.commonWord) {
-                return ctx.commonWord[0];
-            } else if (ctx.keyword) {
-                return ctx.keyword[0];
-            } else if (ctx.punctuation) {
-                return ctx.punctuation[0];
-            }
-        }
+const allTokens = [
+    WhiteSpace,
+    LineTerminator,
+    Comment,
+    Semicolon,
+    LCurly,
+    RCurly,
+    SingleQuote,
+    DoubleQuote,
+    Literal
+]
+class NginxConfigParser extends CstParser {
+    constructor() {
+        super(allTokens);
+        this.performSelfAnalysis();
     }
-    const interpreter = new ExceptInterpreter();
-    const lexer = new Lexer(allTokens);
-
-    const lexingResult = lexer.tokenize(entry);
-    parser.reset();
-    parser.input = lexingResult.tokens;
-    const cst = parser.excerpt();
-    if (parser.errors.length > 0) {
-        return undefined;
+    public config = this.RULE("config", () => {
+        this.SUBRULE(this.directives, { LABEL: "directives" });
     }
-    return interpreter.visit(cst);
+    );
+    private directives = this.RULE("directives", () => {
+        this.MANY(() => this.SUBRULE(this.directive))
+    });
+    private directive = this.RULE("directive", () => {
+        this.CONSUME(Literal);
+        this.SUBRULE(this.parameters);
+        this.OR(
+            [
+                { ALT: () => this.CONSUME(Semicolon) },
+                { ALT: () => this.SUBRULE(this.set) }
 
+            ])
+    });
+    private set = this.RULE("a set of directives", () => {
+        this.CONSUME(LCurly);
+        this.SUBRULE(this.directives);
+        this.CONSUME(RCurly);
+    });
+    private parameters = this.RULE("parameters", () => {
+        this.MANY(
+            () => this.SUBRULE(this.value)
+        )
+    });
+    private value = this.RULE("value", () => {
+        this.OR(
+            [
+                { ALT: () => this.SUBRULE(this.SingleQuotedLiteral) },
+                { ALT: () => this.SUBRULE(this.DoubleQuotedLiteral) },
+                { ALT: () => this.CONSUME(Literal) },
+
+            ]
+        )
+    });
+    private SingleQuotedLiteral = this.RULE("single quoted Literal", () => {
+        this.CONSUME(SingleQuote);
+        this.CONSUME(Literal);
+        this.CONSUME2(SingleQuote);
+    });
+    private DoubleQuotedLiteral = this.RULE("double quoted Literal", () => {
+        this.CONSUME(DoubleQuote);
+        this.CONSUME(Literal);
+        this.CONSUME2(DoubleQuote);
+    });
+}
+
+const parser = new NginxConfigParser();
+const lexer = new Lexer(allTokens);
+
+export const production: Record<string, Rule> = parser.getGAstProductions();
+
+export function parseNginxConfig(text: string) {
+    const lexResult = lexer.tokenize(text)
+    parser.input = lexResult.tokens
+    const cst = parser.config()
+    return {
+        cst,
+        lexErrors: lexResult.errors,
+        parseErrors: parser.errors
+    }
 }
--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
-const assert = require('assert');
-const parser = require('../lib/parser');
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
+const assert = require("assert");
+const { parseNginxConfig } = require("../lib/parser");
+const { NGINX_FULL_EXAMPLE_FILES } = require("./nginx_full_example");
 
-describe('Hello World Test', () => {
-  it('parses', () => {
-    assert.deepEqual(parser.parse('hello **world** then, goodbye world'), [
-      ['**world**'],
-      ['hello', '**world**', 'then', ',', 'goodbye', 'world'],
-    ]);
+describe("Nginx configuration formatter", () => {
+  describe("Parse https://www.nginx.com/resources/wiki/start/topics/examples/full/", () => {
+    it("parses", () => {
+      const parse = (input) => {
+        const parseResult = parseNginxConfig(input);
+        assert.equal(parseResult.lexErrors.length, 0);
+        assert.equal(parseResult.parseErrors.length, 0);
+      };
+      NGINX_FULL_EXAMPLE_FILES.forEach(parse);
+    });
   });
-})
+});
