fe84ed4b7707879a1dff6511f514f94ba481d9fb
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-07-29 11:08:23 +0800

    Compile basic example

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
-const assert = require("assert");
-const greet = require("../lib").default;
+const assert = require('assert');
+const parser = require('../lib/parser');
 
-describe("Hello World Test", () => {
-  describe("test hello", () => {
-    it("greets the world", () => {});
-      assert.notEqual(greet(), "Hello World");
+describe('Hello World Test', () => {
+  it('parses', () => {
+    assert.deepEqual(parser.parse('hello **world** then, goodbye world'), [
+      ['**world**'],
+      ['hello', '**world**', 'then', ',', 'goodbye', 'world'],
+    ]);
   });
-});
+})
